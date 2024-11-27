66535e478a658d749835c039c267bfc828cf6e31
jaykumar <slapbot6@gmail.com>
2024-11-27 04:21:23 -0500

    new sql get

--- seeder/sql/queries/getMangaDetails.sql
+++ seeder/sql/queries/getMangaDetails.sql
@@ -6,12 +6,16 @@ SELECT
     m.original_language,
     m.status,
     ARRAY_AGG(DISTINCT ma.author_id) AS authors,
-    ARRAY_AGG(DISTINCT mar.artist_id) AS artists
+    ARRAY_AGG(DISTINCT mar.artist_id) AS artists,
+    JSONB_AGG(DISTINCT JSONB_BUILD_OBJECT('volume', c.volume, 'chapter', c.chapter, 'title', c.title)) AS chapters,
+    JSONB_AGG(DISTINCT JSONB_BUILD_OBJECT('id', ci.id, 'file_path', ci.file_path)) AS cover_images
 FROM
     manga m
 LEFT JOIN titles t ON m.id = t.manga_id
 LEFT JOIN descriptions d ON m.id = d.manga_id
 LEFT JOIN manga_authors ma ON m.id = ma.manga_id
 LEFT JOIN manga_artists mar ON m.id = mar.manga_id
+LEFT JOIN chapters c ON m.id = c.manga_id
+LEFT JOIN cover_images ci ON m.id = ci.manga_id
 WHERE m.id = $1
 GROUP BY m.id;
