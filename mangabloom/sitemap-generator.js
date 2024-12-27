271d397c0d2dc1c8593772609554e34ad05d55c5
jaykumar <slapbot6@gmail.com>
2024-12-27 17:14:30 -0500

    sitemap generated

--- mangabloom/sitemap-generator.js
+++ mangabloom/sitemap-generator.js
@@ -8,7 +8,7 @@ const routes = [
 
 const generateSitemap = async () => {
   const hostname = "https://yourwebsite.com"; // Replace with your domain
-  const dynamicMangaIds = ["one-piece", "attack-on-titan", "naruto"]; // Replace with actual dynamic fetch logic
+  const dynamicMangaIds = ["75ee72ab-c6bf-4b87-badd-de839156934c", "7f30dfc3-0b80-4dcc-a3b9-0cd746fac005", "c727d921-4294-455b-9bcc-2f0c7d29cd9a"]; // Replace with actual dynamic fetch logic
 
   const sitemap = new SitemapStream({ hostname });
   const writeStream = createWriteStream("./public/sitemap.xml");
